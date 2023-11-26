b8515d7eb97fe168dd26093958d4586887882bad
Alexander Mendivil <alexanderima@outlook.es>
2023-11-25 18:48:57 -0700

    player movement

--- player.gd
+++ player.gd
@@ -12,7 +12,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		increase += 1
-		print(increase)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta 
+	
+	if(Input.is_action_pressed("ui_left")):
+		rotate_z(delta)
+	
+	if(Input.is_action_pressed("ui_right")):
+		rotate_z(-delta)
 	
